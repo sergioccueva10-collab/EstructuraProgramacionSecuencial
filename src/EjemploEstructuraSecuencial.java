f71cf655fe0c450f78909a1af25aa9081d0f0b59
Sergio Cabrera Cueva <Sergio.ccueva.10@gmail.com>
2026-08-11 11:57:31 -0500

    El 11/08/026, y ahora mismo acabo de añadir comentarios que explican las líneas de códigos usadas.

--- src/EjemploEstructuraSecuencial.java
+++ src/EjemploEstructuraSecuencial.java
@@ -26,8 +26,8 @@ public class EjemploEstructuraSecuencial {
 }
 
 //COMENTARIO 2: No olvidar que después de declarar (línea 12), siempre deberá hacerse una asignación (línea 15 y 18).
-/*COMENTARIO 3: Siempre ingresar un 'SOUT' (línea 14 y 17) inmediatamente, porque el usuario será quien vea o realiza
+/*COMENTARIO 3: Siempre ingresar un 'SOUT' (línea 14 y 17) inmediatamente, porque el usuario será quien vea o realice
 alguna acción, es por eso que hay que mostrarle un mensaje indicando qué hacer*/
-//COMENTARIO 4: Siempre realizar las operaciones (líneas 20 y 21), se podría decir que también son asignacioens con propósito.
-/*COMENTARIO 5: Y finalmente, se deberá añadir un mensaje mediante 'SOUT' para indicar al usuario el resultado, como se
-y eso se puede evidenciar en las (líneas 23 y 24)*/
+//COMENTARIO 4: Siempre realizar las operaciones (líneas 20 y 21), se podría decir que también son asignaciones con propósito.
+/*COMENTARIO 5: Y finalmente, se deberá añadir un mensaje mediante 'SOUT' para indicar al usuario el resultado, como
+se puede evidenciar en las (líneas 23 y 24)*/
